e8a27effa2e4d386ec415d5156c75dcb3bef9054
hanhyung6376 <hanhyung6376@naver.com>
2021-10-04 18:23:50 +0900

    React 캘린더

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Calendar from "./containers/Calendar";
+import "./style/calendar.css";
+import "./style/table.css";
+import "./style/modal.css"
+
+
+const App = () => {
+  return (
+    <div>
+      <Calendar/>
+    </div>
+  );
+}
+
+export default App;
